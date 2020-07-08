c8662a245ab945c17c7e141bc3202786c0bf8ebc
Ashish Shukla <ashishshukla160189@gmail.com>
2020-07-08 11:40:05 +0530

    modified files

--- p3.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaProducer
-topicName = 'p3'
-producer = KafkaProducer(bootstrap_servers = ['localhost:9092'])
-producer = KafkaProducer()
-ack = producer.send('p3', ' Demo code for producer....')
-metadata = ack.get()
-print(metadata.topic)
